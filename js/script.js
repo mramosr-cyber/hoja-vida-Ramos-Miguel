fc16507a65c444700017c509b692f59f4da07d78
mramosr-cyber <mramos.r@ucb.edu.bo>
2026-09-12 10:45:02 -0400

    agregado de contenido de js
    implementacion de las funciones para cambiar el modo claro a modo oscuro, y la implementacion de validacion de formulario

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+    const btnTema = document.getElementById('btn_modo');
+    const cuerpoDocumento = document.body;
+
+        btnTema.addEventListener('click', () => {
+            cuerpoDocumento.classList.toggle('modo_oscuro');
+            if (cuerpoDocumento.classList.contains('modo_oscuro')) {
+                btnTema.textContent = '☀️ Modo Claro';
+            } else {
+                btnTema.textContent = '🌙 Modo Oscuro';
+            }
+        });
+
+    const formulario = document.getElementById('formulario_contacto');
+
+    formulario.addEventListener('submit', (evento) => {
+
+        evento.preventDefault();
+
+        const nombre = document.getElementById('nombre').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const mensaje = document.getElementById('mensaje').value.trim();
+        const telefono = document.getElementById('telefono').value.trim();
+        const aceptacion = document.getElementById('aceptacion').checked;
+
+        if (nombre == '' || email == '' || mensaje == '' || telefono == '') {
+            alert('Complete todos los campos de texto obligatorios.');
+            return;
+        }
+
+        // Validación 2: Casilla de términos y condiciones
+        if (!aceptacion) {
+            alert('Debe aceptar las condiciones para enviar su  mensaje.');
+            return; 
+        }
+
+        alert('¡Mensaje enviado con éxito!.');
+        formulario.reset();
+    });
+
+});
